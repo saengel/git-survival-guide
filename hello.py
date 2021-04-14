2fede4e3b0e75f09ec727dcd4d57c31325682d68
saengel <sengel@capitaltg.com>
2021-04-14 09:37:39 -0400

    Adding hello.py file

---
+++ hello.py
@@ -0,0 +1,22 @@
+# Welcome!
+#
+# If you're editing this code locally on whatever Python IDE you prefer, congratulations
+# on cloning into a Git Repo successfully!
+#
+# On a separate branch (and following the instructions in the blog post)
+# edit the code below, and try to make a Pull Request.
+#
+# Some possible modifications:
+# - Add a lengthier message
+# - Add a conditional to the greeting
+# - Add a few other function calls
+#
+# I know this code is very trivial, but that's intentional - so we can focus on Git!
+#
+# Good luck!
+
+
+def greet(name):
+    print(f"Hello, {name}.")
+
+greet("Git")
